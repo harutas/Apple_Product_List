54ec8dcb2045532251ad36fcb34de002d7d33a79
harutas <96802323+harutas@users.noreply.github.com>
2022-05-29 13:00:34 +0900

    Fixed Image path

--- src/product.js
+++ src/product.js
@@ -1,27 +1,27 @@
 const products = {
     'Mac' : [
         {
-            image : '../images/macbook-air.jpeg',
+            image : 'images/macbook-air.jpeg',
             productName : 'MacBook Air',
             price : 115280
         },
         {
-            image : '../images/macbook-pro-13.jpeg',
+            image : 'images/macbook-pro-13.jpeg',
             productName : 'MacBook Pro 13 Inch',
             price : 148280
         },
         {
-            image : '../images/macbook-pro-14.jpeg',
+            image : 'images/macbook-pro-14.jpeg',
             productName : 'MacBook Pro 14 Inch',
             price : 239800
         },
         {
-            image : '../images/macbook-pro-16.jpeg',
+            image : 'images/macbook-pro-16.jpeg',
             productName : 'MacBook Pro 16 Inch',
             price : 299800
         },
         {
-            image : '../images/imac-24.jpeg',
+            image : 'images/imac-24.jpeg',
             productName : 'iMac 24 Inch',
             price : 154800
         }
@@ -29,27 +29,27 @@ const products = {
 
     'iPhone' : [
         {
-            image : '../images/iphone13pro.jpeg',
+            image : 'images/iphone13pro.jpeg',
             productName : 'iPhone 13 Pro',
             price : 122800
         },
         {
-            image : '../images/iphone13.jpeg',
+            image : 'images/iphone13.jpeg',
             productName : 'iPhone 13',
             price : 98800
         },
         {
-            image : '../images/iphonese.jpeg',
+            image : 'images/iphonese.jpeg',
             productName : 'iPhone SE',
             price : 57800
         },
         {
-            image : '../images/iphone12purple.jpeg',
+            image : 'images/iphone12purple.jpeg',
             productName : 'iPhone 12',
             price : 86800
         },
         {
-            image : '../images/iphone11.jpeg',
+            image : 'images/iphone11.jpeg',
             productName : 'iPhone 11',
             price : 61800
         }
@@ -57,22 +57,22 @@ const products = {
 
     'iPad' : [
         {
-            image : '../images/ipad-pro.jpeg',
+            image : 'images/ipad-pro.jpeg',
             productName : 'iPad Pro',
             price : 94800
         },
         {
-            image : '../images/ipad-air.jpeg',
+            image : 'images/ipad-air.jpeg',
             productName : 'iPad Air',
             price : 74800
         },
         {
-            image : '../images/ipad-ipad.jpeg',
+            image : 'images/ipad-ipad.jpeg',
             productName : 'iPad',
             price : 39800
         },
         {
-            image : '../images/ipad-mini.jpeg',
+            image : 'images/ipad-mini.jpeg',
             productName : 'iPad mini',
             price : 59800
         }
@@ -80,27 +80,27 @@ const products = {
 
     'Watch' : [
         {
-            image : '../images/watch-s7.jpeg',
+            image : 'images/watch-s7.jpeg',
             productName : 'Apple Watch Series 7',
             price : 48800
         },
         {
-            image : '../images/watch-se.jpeg',
+            image : 'images/watch-se.jpeg',
             productName : 'Apple Watch SE',
             price : 32800
         },
         {
-            image : '../images/watch-s3.jpeg',
+            image : 'images/watch-s3.jpeg',
             productName : 'Apple Watch Series 3',
             price : 22800
         },
         {
-            image : '../images/watch-nike.jpeg',
+            image : 'images/watch-nike.jpeg',
             productName : 'Apple Watch Nike',
             price : 48800
         },
         {
-            image : '../images/watch-hermes.jpeg',
+            image : 'images/watch-hermes.jpeg',
             productName : 'Apple Watch Hermes',
             price : 141800
         }
